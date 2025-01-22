8aaeeea98003ceac5d6ea86eaf7a3cf1ec25e824
rv810 <vanir1@mcmaster.ca>
2025-01-21 19:07:21 -0500

    Added functions to find path start and end closes #5

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
@@ -2,20 +2,28 @@ package ca.mcmaster.se2aa4.mazerunner;
 public class PathFinder {
     Maze maze = new Maze();
     String[][] maze_arr = maze.getMaze();
+    int[] dimensions = maze.getDimensions();
+    private int[] position;
+    private int[] start_position;
+    private int[] end_position;
 
     protected PathFinder(Maze maze) {
         this.maze = maze;
     }
 
-    protected int[] findStart() {
-        int[] position = new int[2];
-        int[] dimensions = maze.getDimensions();
-
+    protected void findStart() {
         for (int i=0; i<dimensions[0]; i++) {
-            if (maze_arr[i][0] == "PASS") {
-                position = new int[]{i, 0};
+            if (maze_arr[i][0] == "PASS" || maze_arr[i][0].contains("\n") ) {
+                start_position = new int[]{i, 0};
             }
         }
-        return position;
+    }
+
+    protected void findFinish() {
+        for (int i=0; i<dimensions[0]; i++) {
+            if (maze_arr[i][dimensions[1]] == "PASS") {
+                end_position = new int[]{i, dimensions[1]};
+            }
+        }
     }
 }
