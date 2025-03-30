fca5afc057d887761d7a381f854261132eb42535
rv810 <vanir1@mcmaster.ca>
2025-03-29 22:47:50 -0400

    minor changes

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/CheckPathCommand.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/CheckPathCommand.java
@@ -5,7 +5,7 @@ public class CheckPathCommand implements Command {
     Direction startingSide;
     String inputPath;
 
-    protected CheckPathCommand(Maze m, Direction side, String path) {
+    public CheckPathCommand(Maze m, Direction side, String path) {
         this.maze = m;
         this.startingSide = side;
         this.inputPath = path;
